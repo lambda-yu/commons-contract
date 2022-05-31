6b56e856227743f6628c74a781f9caa37792005a
lambda <lambda.yu@hi.com>
2022-05-31 14:26:37 +0800

    add erc20factory

---
+++ contracts/token/ERC20/ERC20Factory.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8;
+
+import "./ERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Factory {
+    using Counters for Counters.Counter;
+    mapping(uint256 => address) private _tokenAddress;
+    mapping(string => uint256) private _tokenId;
+
+    Counters.Counter private _counter;
+
+    constructor() {
+        _counter.increment();
+    }
+
+    event CreateToken(address indexed tokenAddress, uint256 indexed tokenId);
+
+    function create(string calldata tokenName, string calldata tokenSymbol, uint8 tokenDecimal, uint256 tokenTotalSupply) public{
+        require(tokenTotalSupply > 0, "totalsupply must less then zero");
+        require(_tokenId[tokenName] == 0, "this token name is exists");
+
+        bytes memory bytecode = type(ERC20).creationCode;
+        uint256 tokenId = _counter.current();
+
+        bytes32 salt = keccak256(abi.encodePacked(tokenName, tokenSymbol, tokenDecimal, tokenTotalSupply, tokenId));
+        address tokenAddress;
+        assembly {
+            tokenAddress := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        ERC20(tokenAddress).init(tokenName, tokenSymbol, tokenDecimal, tokenTotalSupply, msg.sender); // init
+
+        _tokenId[tokenName] = tokenId;
+        _tokenAddress[tokenId] = tokenAddress;
+
+        emit CreateToken(tokenAddress, tokenId);
+    }
+
+    function getTokenById(uint256 tokenID) public view returns(address) {
+        return _tokenAddress[tokenID];
+    }
+
+    function getTokenByName(string calldata tokenName) public view returns(address) {
+        return _tokenAddress[_tokenId[tokenName]];
+    }
+}
